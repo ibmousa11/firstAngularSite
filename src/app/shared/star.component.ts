1b59ac4a53a70b8dd3ba54bf0d026a2594b8f80c
Ibrahim Mousa <imousa@osisot.com>
2022-06-01 08:21:52 -0700

    Began working on http request functionality

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,33 @@
+import { Input } from "@angular/core";
+import { Output } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { OnChanges } from "@angular/core";
+import { Component } from "@angular/core";
+import { EventEmitter } from "@angular/core";
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+
+})
+
+export class StarComponent implements OnChanges {
+
+  cropWidth: number = 100;
+  @Input() rating: number = 0;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    this.cropWidth = this.rating * 75/5;
+    // console.log(this.rating);
+    // console.log(this.cropWidth);
+  }
+
+  onClick(): void {
+    //console.log(`The rating ${this.rating} was clicked!`);
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
+
+}
